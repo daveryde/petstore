b1458d889d343b248e3aeebb05e82538b0aa5d43
David Cordova <d.mcordova87@gmail.com>
2018-10-24 20:10:37 -0700

    fine tune javascript for services

--- javascript/quote.js
+++ javascript/quote.js
@@ -5,21 +5,22 @@ var start = function() {
 
 // Function that calls on getSelected() to display the prices of the checked items that the user selected in the .cardinfo boxes below
 var services = function() {
-  var selected = getSelected(this.form);
+  var selected = getSelected(form);
   var discounted = calculateDiscount(selected);
 
   results = document.getElementById('card-info1');
   results.innerHTML =
-    '<h2 class="txt-white center">Total prices: $' +
+    '<h2 class="txt-white center">Total price: $' +
     calculatePrice(selected) +
     '</h2>';
 
   sale = document.getElementById('card-info2');
   sale.innerHTML =
-    '<h2 class="txt-white center">Discounted prices: $' + discounted + '</h2>';
+    '<h2 class="txt-white center">Discounted price: $' + discounted + '</h2>';
 };
 
-// Function that accepts a parameter and loops through the "input" tags inside that parameter. We verify whether a check box was checked and push the value to an array variable called items
+// Function that accepts a parameter and loops through the "input" tags inside that parameter.
+// We verify whether a check box was checked and push the value to an array variable called items
 var getSelected = function(form) {
   var items = [];
 
@@ -34,19 +35,20 @@ var getSelected = function(form) {
 };
 
 // Function that accepts a parameter and loops through it's values and returns the sum of each item
-var calculatePrice = function(list) {
+var calculatePrice = function(item) {
   var sum = 0;
 
-  for (var i = 0; i < list.length; i++) {
-    sum = sum + parseInt(list[i]);
+  for (var i = 0; i < item.length; i++) {
+    sum = sum + parseInt(item[i]);
   }
-  return sum;
+  return sum.toFixed(2);
 };
 
+// Function that accepts a parameter value and parses it to an integer
+// then converts and returns the discounted price
 var calculateDiscount = function(item) {
   newPrice = parseInt(item) / 1.25;
-
-  return newPrice;
+  return newPrice.toFixed(2);
 };
 
 window.addEventListener('load', start, false);
