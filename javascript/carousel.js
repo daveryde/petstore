5dc7e057ab3de5cbcec9c85aabc7b7463cd3c5c8
David Cordova <d.mcordova87@gmail.com>
2018-10-28 12:39:21 -0700

    added a javascript carousel to products

---
+++ javascript/carousel.js
@@ -0,0 +1,39 @@
+var slideImg;
+var pictures = ['product-1', 'product-2', 'product-3', 'product-4'];
+var descriptions = ['dog1', 'dog2', 'dog3', 'dog4'];
+index = 0;
+
+// Pick a random image
+function start() {
+  slideImg = document.getElementById('slideImg');
+  var prev = document.getElementById('prev');
+  var next = document.getElementById('next');
+
+  prev.addEventListener('click', reverse, false);
+  next.addEventListener('click', forward, false);
+}
+
+function pickImage(direction) {
+  slideImg.setAttribute('src', '../images/' + pictures[index] + '.jpg');
+  slideImg.setAttribute('alt', descriptions[index]);
+}
+
+function reverse() {
+  if (index == 0) {
+    index = 3;
+  } else {
+    index--;
+  }
+  pickImage(index);
+}
+
+function forward() {
+  if (index == 3) {
+    index = 0;
+  } else {
+    index++;
+  }
+  pickImage(index);
+}
+
+window.addEventListener('load', start, false);
---
+++ javascript/products.js
@@ -0,0 +1,49 @@
+var start = function() {
+  var sectionOne = document.querySelector('.tableOne');
+  var sectionTwo = document.querySelector('.tableTwo');
+
+  var products = [
+    (largeDogs = {
+      service: 'Large Dog Grooming',
+      price: 65
+    }),
+    (smallDogs = {
+      service: 'Small Dog Grooming',
+      price: 50
+    }),
+    (largeCat = {
+      service: 'Large Cat Grooming',
+      price: 45
+    }),
+    (smallCat = {
+      service: 'Small Cat Grooming',
+      price: 35
+    })
+  ];
+
+  // Table template stored in a variable
+  var table =
+    "<table><caption><h2>Pawlished Pets Products</h2></caption><thead><tr><th class='bg-dark txt-white center'>Grooming Services</th><th class='bg-dark txt-white center'>Price</th></tr></thead><tbody>";
+
+  // Table template for orginal price
+  var tableOne = table;
+
+  // Loop through the length of the product arrays and displays
+  // the service/price within the designated table
+  for (var i = 0; i <= products.length - 1; i++) {
+    tableOne +=
+      '<tr><td>' +
+      products[i].service +
+      '</td><td>' +
+      products[i].price +
+      '</td></tr>';
+  }
+
+  // End of table in the HTML document
+  tableOne += '</tbody></table><br><br>';
+
+  // Display the first table
+  sectionOne.innerHTML = tableOne;
+};
+
+window.addEventListener('load', start, false);
